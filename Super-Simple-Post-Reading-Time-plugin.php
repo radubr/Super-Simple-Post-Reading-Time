9819579fda9f7ef5ea2f58517597943fd91dec0f
Radu Braniscan <radu.braniscan@gmail.com>
2024-07-24 23:15:53 +0300

    version 1.1

--- Super-Simple-Post-Reading-Time-plugin.php
+++ Super-Simple-Post-Reading-Time-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Super Simple Post Reading Time
 Plugin URI: https://radubraniscan.com/
 Description: A simple plugin that adds the estimated reading time to the top of every post.
-Version: 1.0
+Version: 1.1
 Author: Radu Braniscan
 Author URI: https://radubraniscan.com/
 License: GPL2
@@ -19,8 +19,14 @@ function calculate_reading_time($content) {
     if (is_single()) {
         $word_count = str_word_count(strip_tags($content));
         $reading_time = ceil($word_count / 200); // Assuming 200 words per minute
-        $reading_time_message = '<p style="font-size: 20px; color: blue;">' . $reading_time . ' min read' . '</p>';
-        $content = $reading_time_message . $content;
+        $reading_time_message = '<span style="font-size: 20px;"> | <i aria-hidden="true" class="far fa-eye"></i> ' . $reading_time . ' min read' . '</span>';
+               // Find the end of the post meta information (usually after the post date)
+        $date_position = strpos($content, '</time>');
+        if ($date_position !== false) {
+            $content = substr_replace($content, $reading_time_message, $date_position + 7, 0);
+        } else {
+            $content = $reading_time_message . $content; // Fallback in case the date is not found
+        }
     }
     return $content;
 }
